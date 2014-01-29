85701f89a1982ffd2568d93afbd4f71be0f824cd
jfarcand <jfarcand@apache.org>
2014-01-29 16:13:08 -0500

    Update Copyright

--- modules/server/src/main/java/org/zodiark/service/db/ValidateService.java
+++ modules/server/src/main/java/org/zodiark/service/db/ValidateService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 High-Level Technologies
+ * Copyright 2013-2014 High-Level Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
